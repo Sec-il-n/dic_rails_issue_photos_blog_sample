a7a106da1d2fe905ddce10385c3a857859be0e95
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-12 09:42:06 +0900

    fixup! 非ログインユーザのバリデーションと遷移、エラー表示の修正

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,7 @@
 module BlogsHelper
    def choose_new_or_edit
-     if action_name == 'new'|| action_name == 'create'
+     if action_name == 'show'|| action_name == 'create'
+     # if action_name == 'new'|| action_name == 'create'
        confirm_blogs_path
      elsif action_name == 'edit'
        blog_path
