3093f6293d6423fe4c7bcb3e72a51e328474fc38
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-16 06:19:13 +0900

    トップページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-<<<<<<< HEAD
   before_action :authenticate_user
   def authenticate_user
     if current_user == nil
-=======
-  #エラーが出るので消している　↓undefined local variable or method `flash' for ApplicationController:Class
-  before_action :authenticate_user
-  def authenticate_user
-    if current_user == nil
-    # if @currentuser == nil
-      # flash[:notice]=t('notice.login_needed')
->>>>>>> セッション管理、非ログインユーザーの処理
       redirect_to new_session_path, notice:'ログインしてください'
     end
   end
